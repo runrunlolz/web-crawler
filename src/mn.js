8f6cdbc142b9e802ae1927df88d7e6ba6be29284
runrunlolz <runnan007@126.com>
2025-03-06 19:47:30 +0800

    文字修改

--- src/mn.js
+++ src/mn.js
@@ -3,22 +3,23 @@ const { mn } = require('./config/default');
 const srcToImg = require('./helper/srcToimg');
 
 (async () => {
+    console.log(123);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://image.baidu.com');
     console.log('go to https://image.baidu.com');
-    
+
     await page.setViewport({
         width: 1920,
         height: 1080
     });
     console.log('reset viewport');
-    
+
     await page.focus('#kw');
     await page.keyboard.sendCharacter('猫');
     await page.click('.s_search');
     console.log('go to search list');
-    
+
     page.on('load', async () => {
         console.log('page loading dong, stat fetch...');
 
@@ -27,7 +28,7 @@ const srcToImg = require('./helper/srcToimg');
             return Array.prototype.map.call(images, img => img.src);
         });
         console.log(`get ${srcs.length} images, start download`);
-        
+
         srcs.forEach(async src => {
             //sleep
             await page.waitFor(200);
@@ -35,10 +36,10 @@ const srcToImg = require('./helper/srcToimg');
         });
 
         // const srcs = await page.$$eval('.main_img', images => images.length
-            
+
         // )
         // console.log(srcs);
-        
+
         await browser.close();
     });
 
